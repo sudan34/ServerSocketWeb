5c33734f992ab2a2a29f1590246e966ab338b043
Madhu sudan Bhattarai <83052090+sudan34@users.noreply.github.com>
2023-09-05 15:59:46 +0545

    changes

---
+++ ClientSocket/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    private const string ServerIp = "127.0.0.1"; // Change to the server's IP address
+    private const int ServerPort = 7022; // Change to the server's port
+
+    static async Task Main(string[] args)
+    {
+        await StartClientAsync();
+    }
+
+    static async Task StartClientAsync()
+    {
+        using var clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        try
+        {
+            clientSocket.Connect(new IPEndPoint(IPAddress.Parse(ServerIp), ServerPort));
+
+            Console.WriteLine("Connected to the server.");
+
+            while (true)
+            {
+                Console.Write("Enter a message to send to the server (or type 'exit' to quit): ");
+                string messageToSend = Console.ReadLine();
+
+                if (messageToSend.ToLower() == "exit")
+                {
+                    break; // Exit the loop and close the client
+                }
+
+                Console.WriteLine($"Sending to server: {messageToSend}");
+                byte[] messageBytes = Encoding.ASCII.GetBytes(messageToSend);
+                await clientSocket.SendAsync(new ArraySegment<byte>(messageBytes), SocketFlags.None);
+
+                // Receive and display the server's response.
+                var responseBuffer = new byte[1024];
+                int bytesRead = await clientSocket.ReceiveAsync(new ArraySegment<byte>(responseBuffer), SocketFlags.None);
+                if (bytesRead > 0)
+                {
+                    string serverResponse = Encoding.ASCII.GetString(responseBuffer, 0, bytesRead);
+                    Console.WriteLine($"Received from server: {serverResponse}");
+                }
+                else
+                {
+                    Console.WriteLine("Server closed the connection.");
+                    break;
+                }
+            }
+            // Close the client socket when done.
+            clientSocket.Close();
+        }
+        catch (SocketException ex)
+        {
+            Console.WriteLine($"SocketException: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
---
+++ ServerSocket/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    private const int Port = 7022;
+
+    static void Main(string[] args)
+    {
+        StartServer();
+    }
+
+    static void StartServer()
+    {
+        try
+        {
+            // Create a TCP/IP socket.
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // Bind the socket to the local endpoint and port.
+            listener.Bind(new IPEndPoint(IPAddress.Any, Port));
+
+            // Start listening for incoming connections.
+            listener.Listen(10); // Maximum 10 pending connections
+
+            Console.WriteLine($"Server is listening on port {Port}...");
+
+            while (true)
+            {
+                // Accept an incoming connection.
+                Socket clientSocket = listener.Accept();
+
+                // Start a new thread to handle the client communication.
+                System.Threading.Thread clientThread = new System.Threading.Thread(() =>
+                {
+                    HandleClient(clientSocket);
+                });
+                clientThread.Start();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Server error: {ex.Message}");
+        }
+    }
+
+    static void HandleClient(Socket clientSocket)
+    {
+        try
+        {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = clientSocket.Receive(buffer)) > 0)
+            {
+                string clientMessage = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Received from client: {clientMessage}");
+
+                // Process the client message and prepare a response.
+                string responseMessage = $"Server Response: {clientMessage}";
+                Console.WriteLine($"Sending to client: {responseMessage}");
+
+                // Send the response back to the client.
+                clientSocket.Send(Encoding.ASCII.GetBytes(responseMessage));
+            }
+
+            // Close the client socket when done.
+            clientSocket.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
